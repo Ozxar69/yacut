da9d53df5145204394f42977248b60e644c719b5
Алексей Волков <alek.bishop@yandex.ru>
2023-12-20 12:33:01 +0300

    1

--- yacut/models.py
+++ yacut/models.py
@@ -1,7 +1,18 @@
 from datetime import datetime
 
+from flask import url_for
+
 from yacut import db
 
 
 class URLMap(db.Model):
-    ...
+    id = db.Column(db.Integer, primary_key=True)
+    original = db.Column(db.String(256), nullable=False)
+    short = db.Column(db.String(64), unique=True)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+
+    def to_dict(self):
+        ...
+
+    def from_dict(self):
+        ...
